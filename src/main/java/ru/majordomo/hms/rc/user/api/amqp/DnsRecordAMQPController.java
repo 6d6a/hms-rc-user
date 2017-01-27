ca871bd4af69b0943fe53e774f86e49941c601d3
Алексей Заборщиков <zaborshikov@majordomo.ru>
2017-01-27 12:47:12 +0300

    dns amqp controller

---
+++ src/main/java/ru/majordomo/hms/rc/user/api/amqp/DnsRecordAMQPController.java
@@ -0,0 +1,67 @@
+package ru.majordomo.hms.rc.user.api.amqp;
+
+import org.springframework.amqp.rabbit.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+import ru.majordomo.hms.rc.user.api.message.ServiceMessage;
+import ru.majordomo.hms.rc.user.managers.GovernorOfDnsRecord;
+
+@EnableRabbit
+@Service
+public class DnsRecordAMQPController extends BaseAMQPController {
+
+    @Autowired
+    public void setGovernor(GovernorOfDnsRecord governor) {
+        this.governor = governor;
+    }
+
+    @RabbitListener(bindings = @QueueBinding(value = @Queue(value = "${spring.application.name}.dns-record.create",
+            durable = "true", autoDelete = "true"),
+            exchange = @Exchange(value = "dns-record.create", type = "topic"),
+            key = "rc.user"))
+    public void handleCreateEvent(@Header(value = "provider", required = false) String eventProvider,
+                                  @Payload ServiceMessage serviceMessage) {
+        switch (eventProvider) {
+            case ("pm"):
+                handleCreateEventFromPM("dns-record", serviceMessage);
+                break;
+            case ("te"):
+                handleCreateEventFromTE("dns-record", serviceMessage);
+                break;
+        }
+    }
+
+    @RabbitListener(bindings = @QueueBinding(value = @Queue(value = "${spring.application.name}.dns-record.update",
+            durable = "true", autoDelete = "true"),
+            exchange = @Exchange(value = "dns-record.update", type = "topic"),
+            key = "rc.user"))
+    public void handleUpdateEvent(@Header(value = "provider", required = false) String eventProvider,
+                                  @Payload ServiceMessage serviceMessage) {
+        switch (eventProvider) {
+            case ("pm"):
+                handleUpdateEventFromPM("dns-record", serviceMessage);
+                break;
+            case ("te"):
+                handleUpdateEventFromTE("dns-record", serviceMessage);
+                break;
+        }
+    }
+
+    @RabbitListener(bindings = @QueueBinding(value = @Queue(value = "${spring.application.name}.dns-record.delete",
+            durable = "true", autoDelete = "true"),
+            exchange = @Exchange(value = "dns-record.delete", type = "topic"),
+            key = "rc.user"))
+    public void handleDeleteEvent(@Header(value = "provider", required = false) String eventProvider,
+                                  @Payload ServiceMessage serviceMessage) {
+        switch (eventProvider) {
+            case ("pm"):
+                handleDeleteEventFromPM("dns-record", serviceMessage);
+                break;
+            case ("te"):
+                handleDeleteEventFromTE("dns-record", serviceMessage);
+                break;
+        }
+    }
+}
