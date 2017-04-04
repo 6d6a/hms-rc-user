01ed4fdc7f609b63bcae37b107855548160abbd6
Ilya Dolnigin <dolnigin@majordomo.ru>
2017-04-04 16:34:50 +0300

    FeignConfig (interceptor) / ResourceServerConfig / url changed in getActiveMailboxStorageByServerId

---
+++ src/main/java/ru/majordomo/hms/rc/user/configurations/FeignConfig.java
@@ -0,0 +1,64 @@
+package ru.majordomo.hms.rc.user.configurations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.AccessTokenRequest;
+import org.springframework.security.oauth2.client.token.DefaultAccessTokenRequest;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
+import org.springframework.web.context.request.RequestContextListener;
+
+import java.util.Collections;
+
+import feign.RequestInterceptor;
+
+@Configuration
+public class FeignConfig {
+    @Value("${security.oauth2.client.accessTokenUri}")
+    private String accessTokenUri;
+
+    @Value("${security.oauth2.client.clientId}")
+    private String clientId;
+
+    @Value("${security.oauth2.client.clientSecret}")
+    private String clientSecret;
+
+    @Value("${security.oauth2.client.scope}")
+    private String scope;
+
+    @Value("${si_oauth.serviceUsername}")
+    private String username;
+
+    @Value("${si_oauth.servicePassword}")
+    private String password;
+
+    @Bean
+    public RequestInterceptor oauth2FeignRequestInterceptor(){
+        AccessTokenRequest accessTokenRequest = new DefaultAccessTokenRequest();
+        OAuth2ClientContext oAuth2ClientContext = new DefaultOAuth2ClientContext(accessTokenRequest);
+
+        return new OAuth2FeignRequestInterceptor(oAuth2ClientContext, resource());
+    }
+
+    private OAuth2ProtectedResourceDetails resource() {
+        ResourceOwnerPasswordResourceDetails details = new ResourceOwnerPasswordResourceDetails();
+        details.setAccessTokenUri(accessTokenUri);
+        details.setClientId(clientId);
+        details.setClientSecret(clientSecret);
+        details.setScope(Collections.singletonList(scope));
+        details.setGrantType("password");
+        details.setUsername(username);
+        details.setPassword(password);
+
+        return details;
+    }
+
+    @Bean
+    public RequestContextListener requestContextListener(){
+        return new RequestContextListener();
+    }
+}
---
+++ src/main/java/ru/majordomo/hms/rc/user/configurations/ResourceServerConfig.java
@@ -0,0 +1,25 @@
+package ru.majordomo.hms.rc.user.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable();
+    }
+}
