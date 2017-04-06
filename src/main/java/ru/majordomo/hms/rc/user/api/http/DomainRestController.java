6ae6c283eeebebf7a7afa1bf15af4e15901a252b
Алексей Заборщиков <zaborshikov@majordomo.ru>
2017-04-06 19:14:45 +0300

    fixed saving dns record

--- src/main/java/ru/majordomo/hms/rc/user/api/http/DomainRestController.java
+++ src/main/java/ru/majordomo/hms/rc/user/api/http/DomainRestController.java
@@ -1,5 +1,7 @@
 package ru.majordomo.hms.rc.user.api.http;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import ru.majordomo.hms.rc.user.managers.GovernorOfDnsRecord;
 import ru.majordomo.hms.rc.user.managers.GovernorOfDomain;
 import ru.majordomo.hms.rc.user.resources.DNSResourceRecord;
 import ru.majordomo.hms.rc.user.resources.Domain;
@@ -17,6 +20,11 @@ import ru.majordomo.hms.rc.user.resources.Domain;
 public class DomainRestController {
 
     private GovernorOfDomain governor;
+
+    @Autowired
+    private GovernorOfDnsRecord governorOfDnsRecord;
+
+    private final static Logger logger = LoggerFactory.getLogger(DomainRestController.class);
 
     @Autowired
     public void setGovernor(GovernorOfDomain governor) {
@@ -33,12 +41,13 @@ public class DomainRestController {
         try {
             Map<String, String> keyValue = new HashMap<>();
             keyValue.put("name", domainName);
+            logger.info("adding DNS-record for domain " + domainName);
             Domain domain = governor.build(keyValue);
             if (domain.getParentDomainId() != null)
                 domain = governor.build(domain.getParentDomainId());
-            domain.addDnsResourceRecord(record);
-            governor.validate(domain);
-            governor.store(domain);
+            record.setName(domain.getName());
+            governorOfDnsRecord.validate(record);
+            governorOfDnsRecord.store(record);
             return new ResponseEntity(HttpStatus.ACCEPTED);
         } catch (Exception e) {
             return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
