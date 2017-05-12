ac3189d21b11d4c38f02e7f5602232ca8192cb7b
Nikita Klimov <klimov@majordomo.ru>
2017-05-12 13:24:34 +0300

    Use default mongo converters for Passport and LegalEntity

--- src/main/java/ru/majordomo/hms/rc/user/configurations/MongoConfig.java
+++ src/main/java/ru/majordomo/hms/rc/user/configurations/MongoConfig.java
@@ -2,12 +2,18 @@ package ru.majordomo.hms.rc.user.configurations;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.data.mongodb.core.convert.CustomConversions;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import ru.majordomo.hms.rc.user.mappers.PersonWriteConverter;
 
 import java.util.ArrayList;
@@ -18,7 +24,13 @@ public class MongoConfig {
     @Bean
     public CustomConversions customConversions() {
         List<Converter<?, ?>> converterList = new ArrayList<Converter<?, ?>>();
-        converterList.add(new PersonWriteConverter());
+        converterList.add(new PersonWriteConverter(objectFactory()));
         return new CustomConversions(converterList);
+    }
+
+    @Bean
+    public ObjectFactory<MappingMongoConverter> objectFactory() {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(new SimpleMongoDbFactory(new MongoClient(), "usersResourceController"));
+        return () -> new MappingMongoConverter(dbRefResolver, new MongoMappingContext());
     }
 }
--- src/main/java/ru/majordomo/hms/rc/user/mappers/PersonWriteConverter.java
+++ src/main/java/ru/majordomo/hms/rc/user/mappers/PersonWriteConverter.java
@@ -2,12 +2,22 @@ package ru.majordomo.hms.rc.user.mappers;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.convert.WritingConverter;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import ru.majordomo.hms.rc.user.resources.Person;
 
+import javax.annotation.Resource;
+
 @WritingConverter
-public class PersonWriteConverter implements Converter<Person, DBObject>{
+public class PersonWriteConverter implements Converter<Person, DBObject> {
+    private ObjectFactory<MappingMongoConverter> mappingMongoConverterObjectFactory;
+
+    public PersonWriteConverter(ObjectFactory<MappingMongoConverter> mappingMongoConverterObjectFactory) {
+        this.mappingMongoConverterObjectFactory = mappingMongoConverterObjectFactory;
+    }
+
     @Override
     public DBObject convert(Person source) {
         DBObject dbo = new BasicDBObject();
@@ -17,8 +27,8 @@ public class PersonWriteConverter implements Converter<Person, DBObject>{
         dbo.put("switchedOn", source.getSwitchedOn());
         dbo.put("emailAdresses", source.getEmailAddresses());
         dbo.put("phoneNumbers", source.getPhoneNumbers());
-        dbo.put("passport", source.getPassport());
-        dbo.put("legalEntity", source.getLegalEntity());
+        dbo.put("passport", mappingMongoConverterObjectFactory.getObject().convertToMongoType(source.getPassport()));
+        dbo.put("legalEntity", mappingMongoConverterObjectFactory.getObject().convertToMongoType(source.getLegalEntity()));
         dbo.put("country", source.getCountry());
         dbo.put("postalAddress", source.getPostalAddressAsString());
         dbo.put("nicHandle", source.getNicHandle());
