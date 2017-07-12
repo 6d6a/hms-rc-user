be8315212bad0aa34f5b8707c740961c25c55134
Алексей Заборщиков <zaborshikov@majordomo.ru>
2017-07-12 13:47:53 +0300

    use Apache Common Validator for validating domain name

--- src/main/java/ru/majordomo/hms/rc/user/resources/validation/validator/DomainNameValidator.java
+++ src/main/java/ru/majordomo/hms/rc/user/resources/validation/validator/DomainNameValidator.java
@@ -2,12 +2,16 @@ package ru.majordomo.hms.rc.user.resources.validation.validator;
 
 import com.google.common.net.InternetDomainName;
 
+import org.apache.commons.validator.routines.DomainValidator;
 import org.springframework.stereotype.Component;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import ru.majordomo.hms.rc.user.resources.validation.DomainName;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 public class DomainNameValidator implements ConstraintValidator<DomainName, String> {
@@ -19,6 +23,8 @@ public class DomainNameValidator implements ConstraintValidator<DomainName, Stri
     @Override
     public boolean isValid(String domainName, ConstraintValidatorContext constraintValidatorContext) {
         try {
+            if (!DomainValidator.getInstance().isValid(domainName)) return false;
+
             InternetDomainName domain = InternetDomainName.from(domainName);
             String domainPublicPart = domain.publicSuffix().toString();
         } catch (Exception e) {
