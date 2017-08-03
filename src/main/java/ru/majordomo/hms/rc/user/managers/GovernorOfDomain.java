c5a56394bfc7feba5d54eccaf901c4a177fce8d6
Алексей Заборщиков <zaborshikov@majordomo.ru>
2017-08-03 12:31:50 +0300

    dummy

--- src/main/java/ru/majordomo/hms/rc/user/managers/GovernorOfDomain.java
+++ src/main/java/ru/majordomo/hms/rc/user/managers/GovernorOfDomain.java
@@ -255,6 +255,7 @@ public class GovernorOfDomain extends LordOfResources<Domain> {
             } else {
                 throw new ParameterValidateException("Персона должна быть указана");
             }
+            
             domain.setAutoRenew(true);
         } else if (serviceMessage.getParam("parentDomainId") != null) {
             Domain parent = repository.findOne((String) serviceMessage.getParam("parentDomainId"));
