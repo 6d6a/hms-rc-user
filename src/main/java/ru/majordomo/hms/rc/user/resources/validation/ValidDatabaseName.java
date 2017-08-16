3abc2425eb80bec7257a892e3f0fc13d2aa2c129
Березка Валентин <berezka@majordomo.ru>
2017-08-16 16:34:42 +0300

    мелкие правки

--- src/main/java/ru/majordomo/hms/rc/user/resources/validation/ValidDatabaseName.java
+++ src/main/java/ru/majordomo/hms/rc/user/resources/validation/ValidDatabaseName.java
@@ -10,11 +10,10 @@ import java.lang.annotation.*;
 
 @Documented
 @Constraint(validatedBy = {})
-@Length(max=64, message = "Имя не может быть длиннее 16 символов", groups = { DatabaseChecks.class})
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.TYPE_USE})
 @Retention(RetentionPolicy.RUNTIME)
 @Pattern(regexp = "[a-zA-Z0-9_]+", message = "{ru.majordomo.hms.rc.user.resources.validation.DatabaseName.message}")
-
+@Length(max=64, message = "Имя не может быть длиннее 64 символов")
 public @interface ValidDatabaseName {
     String message() default "{ru.majordomo.hms.rc.user.resources.validation.DatabaseName.message}";
 
