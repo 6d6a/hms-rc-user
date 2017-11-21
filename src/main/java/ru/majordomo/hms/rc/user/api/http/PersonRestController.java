48a5bb851186a3af223ddf362b30a9d8925ea61e
Влади́слав Пинаев <pinaev@majordomo.ru>
2017-11-21 18:33:39 +0300

    Ручная синхронизация персоны

--- src/main/java/ru/majordomo/hms/rc/user/api/http/PersonRestController.java
+++ src/main/java/ru/majordomo/hms/rc/user/api/http/PersonRestController.java
@@ -1,15 +1,22 @@
 package ru.majordomo.hms.rc.user.api.http;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import ru.majordomo.hms.rc.user.exception.ParameterValidateException;
+import ru.majordomo.hms.rc.user.exception.ResourceNotFoundException;
 import ru.majordomo.hms.rc.user.managers.GovernorOfPerson;
 import ru.majordomo.hms.rc.user.resources.Person;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 
 @RestController
 public class PersonRestController {
@@ -44,6 +51,23 @@ public class PersonRestController {
         Map<String, String> keyValue = new HashMap<>();
         keyValue.put("accountId", accountId);
         return governor.buildAll(keyValue);
+    }
+
+    @RequestMapping(value = {"{accountId}/person/{personId}/sync", "{accountId}/person/{personId}/sync/"}, method = RequestMethod.GET)
+    public ResponseEntity syncPerson(@PathVariable("accountId") String accountId,@PathVariable("personId") String personId) {
+        Map<String, String> keyValue = new HashMap<>();
+        keyValue.put("resourceId", personId);
+        keyValue.put("accountId", accountId);
+        Person person = governor.build(keyValue);
+
+        try {
+            governor.manualSync(person);
+        } catch (ConstraintViolationException e) {
+            return new ResponseEntity<>("Ошибка при валидации персоны", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+
     }
 
     @RequestMapping(value = "/{accountId}/person", method = RequestMethod.POST)
