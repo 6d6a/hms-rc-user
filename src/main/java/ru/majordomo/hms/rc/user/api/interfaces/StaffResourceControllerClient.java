f98f3284e5c9d136415485e024c646aae84819d1
Ilya Dolnigin <dolnigin@majordomo.ru>
2019-01-24 11:41:46 +0300

    cacheable import

--- src/main/java/ru/majordomo/hms/rc/user/api/interfaces/StaffResourceControllerClient.java
+++ src/main/java/ru/majordomo/hms/rc/user/api/interfaces/StaffResourceControllerClient.java
@@ -1,5 +1,6 @@
 package ru.majordomo.hms.rc.user.api.interfaces;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
