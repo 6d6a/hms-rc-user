389deae945666cb5b08bf870538e35652629fcc1
Валентин Березка <berezka@majordomo.ru>
2019-04-22 10:23:41 +0000

    добавлены разрешенные ip для mailbox

--- src/main/java/ru/majordomo/hms/rc/user/resources/Mailbox.java
+++ src/main/java/ru/majordomo/hms/rc/user/resources/Mailbox.java
@@ -2,8 +2,12 @@ package ru.majordomo.hms.rc.user.resources;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.hibernate.validator.constraints.Length;
 import javax.validation.constraints.NotBlank;
+
+import org.junit.Assert;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -12,16 +16,21 @@ import ru.majordomo.hms.rc.user.resources.validation.*;
 
 import java.io.UnsupportedEncodingException;
 import java.net.IDN;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
+@Data
+@EqualsAndHashCode(callSuper = true)
 @Document(collection = "mailboxes")
 @ValidMailbox
 public class Mailbox extends Resource implements ServerStorable, Quotable, Securable {
+    @JsonIgnore
+    private final static String CIDR_PATTERN = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])/([1-9]|[1-2]\\d|3[0-2])$";
+
     @Transient
     private Domain domain;
 
@@ -77,13 +86,11 @@ public class Mailbox extends Resource implements ServerStorable, Quotable, Secur
     @NotNull(message = "Uid не может быть равным null")
     private Integer uid;
 
+    private Set<@Pattern(regexp = CIDR_PATTERN, message = "{ru.majordomo.hms.rc.user.resources.validation.ValidCidr.message}") String> allowedIps = new HashSet<>();
+
     @Override
     public void switchResource() {
         switchedOn = !switchedOn;
-    }
-
-    public Domain getDomain() {
-        return domain;
     }
 
     public void setDomain(Domain domain) {
@@ -109,30 +116,10 @@ public class Mailbox extends Resource implements ServerStorable, Quotable, Secur
         }
     }
 
-    public String getPasswordHash() {
-        return passwordHash;
-    }
-
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
-    }
-
     public void setPasswordHashByPlainPassword(String plainPassword) throws UnsupportedEncodingException {
         if (plainPassword != null && !plainPassword.equals("")) {
             passwordHash = PasswordManager.forMailStorage(plainPassword);
         }
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public List<String> getRedirectAddresses() {
-        return redirectAddresses;
     }
 
     @JsonIgnore
@@ -140,16 +127,8 @@ public class Mailbox extends Resource implements ServerStorable, Quotable, Secur
         return getPunycodedList(redirectAddresses);
     }
 
-    public void setRedirectAddresses(List<String> redirectAddresses) {
-        this.redirectAddresses = redirectAddresses;
-    }
-
     public void addRedirectAddress(String emailAddress) {
         this.redirectAddresses.add(emailAddress);
-    }
-
-    public List<String> getBlackList() {
-        return blackList;
     }
 
     @JsonIgnore
@@ -157,25 +136,13 @@ public class Mailbox extends Resource implements ServerStorable, Quotable, Secur
         return this.getPunycodedList(blackList);
     }
 
-    public void setBlackList(List<String> blackList) {
-        this.blackList = blackList;
-    }
-
     public void addToBlackList(String emailAddress) {
         this.blackList.add(emailAddress);
-    }
-
-    public List<String> getWhiteList() {
-        return whiteList;
     }
 
     @JsonIgnore
     public List<String> getWhiteListInPunycode() {
         return this.getPunycodedList(whiteList);
-    }
-
-    public void setWhiteList(List<String> whiteList) {
-        this.whiteList = whiteList;
     }
 
     public void addToWhiteList(String emailAddress) {
@@ -199,78 +166,9 @@ public class Mailbox extends Resource implements ServerStorable, Quotable, Secur
         return punycidedList;
     }
 
-    public Boolean getMailFromAllowed() { return mailFromAllowed; }
-
-    public void setMailFromAllowed(Boolean mailFromAllowed) { this.mailFromAllowed = mailFromAllowed; }
-
-    public Boolean getAntiSpamEnabled() {
-        return antiSpamEnabled;
-    }
-
-    public void setAntiSpamEnabled(Boolean antiSpamEnabled) {
-        this.antiSpamEnabled = antiSpamEnabled;
-    }
-
-    public SpamFilterAction getSpamFilterAction() {
-        return spamFilterAction;
-    }
-
-    public void setSpamFilterAction(SpamFilterAction spamFilterAction) {
-        this.spamFilterAction = spamFilterAction;
-    }
-
-    public SpamFilterMood getSpamFilterMood() {
-        return spamFilterMood;
-    }
-
-    public void setSpamFilterMood(SpamFilterMood spamFilterMood) {
-        this.spamFilterMood = spamFilterMood;
-    }
-
-    public String getDomainId() {
-        return domainId;
-    }
-
-    public void setDomainId(String domainId) {
-        this.domainId = domainId;
-    }
-
-    @Override
-    public void setQuota(Long quota) {
-        this.quota = quota;
-    }
-
-    @Override
-    public Long getQuota() { return quota; }
-
-    @Override
-    public void setQuotaUsed(Long quotaUsed) {
-        this.quotaUsed = quotaUsed;
-    }
-
     @Override
     public Long getQuotaUsed() {
         return quotaUsed == null ? 0L : quotaUsed;
-    }
-
-    @Override
-    public void setWritable(Boolean writable) {
-        this.writable = writable;
-    }
-
-    @Override
-    public Boolean getWritable() {
-        return writable;
-    }
-
-    @Override
-    public String getServerId() {
-        return serverId;
-    }
-
-    @Override
-    public void setServerId(String serverId) {
-        this.serverId = serverId;
     }
 
     public Boolean getIsAggregator() {
@@ -279,10 +177,6 @@ public class Mailbox extends Resource implements ServerStorable, Quotable, Secur
 
     public void setIsAggregator(Boolean isAggregator) {
         this.isAggregator = isAggregator;
-    }
-
-    public String getMailSpool() {
-        return mailSpool;
     }
 
     @JsonIgnore
@@ -294,41 +188,5 @@ public class Mailbox extends Resource implements ServerStorable, Quotable, Secur
         }
         String mailSpoolinPunycode = String.join("/", labels);
         return String.join("/", mailSpoolinPunycode);
-    }
-
-    public void setMailSpool(String mailSpool) {
-        this.mailSpool = mailSpool;
-    }
-
-    public Integer getUid() {
-        return uid;
-    }
-
-    public void setUid(Integer uid) {
-        this.uid = uid;
-    }
-
-    @Override
-    public String toString() {
-        return "Mailbox{" +
-                "domain=" + domain +
-                ", domainId='" + domainId + '\'' +
-                ", passwordHash='" + passwordHash + '\'' +
-                ", comment='" + comment + '\'' +
-                ", redirectAddresses=" + redirectAddresses +
-                ", blackList=" + blackList +
-                ", whiteList=" + whiteList +
-                ", mailFromAllowed=" + mailFromAllowed +
-                ", antiSpamEnabled=" + antiSpamEnabled +
-                ", spamFilterAction=" + spamFilterAction +
-                ", spamFilterMood=" + spamFilterMood +
-                ", serverId='" + serverId + '\'' +
-                ", quota=" + quota +
-                ", quotaUsed=" + quotaUsed +
-                ", writable=" + writable +
-                ", isAggregator=" + isAggregator +
-                ", mailSpool='" + mailSpool + '\'' +
-                ", uid=" + uid +
-                "} " + super.toString();
     }
 }
