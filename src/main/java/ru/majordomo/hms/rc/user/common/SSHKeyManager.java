7d91c954c86bcec5e79698312f63506c5f389946
Андрианова Ольга <andrianov@majordomo.ru>
2020-08-31 14:21:35 +0000

    убивать процесс puttygen если слишком долго не завершается

--- src/main/java/ru/majordomo/hms/rc/user/common/SSHKeyManager.java
+++ src/main/java/ru/majordomo/hms/rc/user/common/SSHKeyManager.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +21,8 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @Slf4j
 @ParametersAreNonnullByDefault
 public class SSHKeyManager {
+    private final static int WAIN_TIMEOUT = 120000;
+
     public static SSHKeyPair generateKeyPair() throws JSchException {
         ByteArrayOutputStream privateKey = new ByteArrayOutputStream();
         ByteArrayOutputStream publicKey = new ByteArrayOutputStream();
@@ -43,14 +46,21 @@ public class SSHKeyManager {
             pemFile = Files.createTempFile("rc-user_pem2ppk", "");
             Files.write(pemFile, Collections.singletonList(privateKeyPem));
             String ppkName = pemFile.toAbsolutePath().toString() + ".ppk";
-            Process puttygen = new ProcessBuilder().command(puttygenPath, pemFile.toAbsolutePath().toString(), "-o", ppkName).start();
-            int exitCode = puttygen.waitFor();
+            Process puttygen = new ProcessBuilder()
+                    .command(puttygenPath, pemFile.toAbsolutePath().toString(), "-q", "-o", ppkName).start();
+            puttygen.waitFor(WAIN_TIMEOUT, TimeUnit.MILLISECONDS);
             ppkFile = Paths.get(ppkName);
+            if (puttygen.isAlive()) {
+                puttygen.destroy();
+                return null;
+            }
+            int exitCode = puttygen.exitValue();
             if (exitCode == 0) {
                 return Files.readAllBytes(ppkFile);
             } else {
-                String errorLines = new BufferedReader(new InputStreamReader(puttygen.getErrorStream())).lines().collect(Collectors.joining(String.format("%n")));
-                log.error(String.format("puttygen returned error code: %d. Error output: %n%s%n", exitCode, errorLines));
+                String errorLines = new BufferedReader(new InputStreamReader(puttygen.getErrorStream())).lines()
+                        .filter(s -> !s.trim().isEmpty()).collect(Collectors.joining(String.format("%n")));
+                log.error(String.format("puttygen returned error code: %d. Error output: %n%s", exitCode, errorLines));
                 return null;
             }
         } finally {
