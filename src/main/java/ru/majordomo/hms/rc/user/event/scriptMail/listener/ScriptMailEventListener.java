1a5168bd5631148eb238e41497914f20d8d166d4
Порунов Дмитрий <porunov@majordomo.ru>
2020-09-24 13:55:40 +0000

    Issue 7940

--- src/main/java/ru/majordomo/hms/rc/user/event/scriptMail/listener/ScriptMailEventListener.java
+++ src/main/java/ru/majordomo/hms/rc/user/event/scriptMail/listener/ScriptMailEventListener.java
@@ -1,4 +1,4 @@
-package ru.majordomo.hms.rc.user.event.infect.listener;
+package ru.majordomo.hms.rc.user.event.scriptMail.listener;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
@@ -6,17 +6,17 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import ru.majordomo.hms.rc.user.api.interfaces.PmFeignClient;
 import ru.majordomo.hms.rc.user.api.message.ServiceMessage;
-import ru.majordomo.hms.rc.user.event.infect.UnixAccountInfectNotifyEvent;
+import ru.majordomo.hms.rc.user.event.scriptMail.UnixAccountScriptMailNotifyEvent;
 import ru.majordomo.hms.rc.user.resources.UnixAccount;
 
 import static ru.majordomo.hms.rc.user.common.Constants.*;
 
 @Component
-public class InfectEventListener {
+public class ScriptMailEventListener {
     private PmFeignClient personmgr;
-
+    
     @Autowired
-    public InfectEventListener(
+    public ScriptMailEventListener(
             PmFeignClient pmFeignClient
     ) {
         personmgr = pmFeignClient;
@@ -24,7 +24,7 @@ public class InfectEventListener {
 
     @EventListener
     @Async("threadPoolTaskExecutor")
-    public void onInfectEvent(UnixAccountInfectNotifyEvent event) {
+    public void onScriptMailEvent(UnixAccountScriptMailNotifyEvent event) {
         String accountId = event.getSource();
         convertAndSendEmail(accountId);
     }
@@ -32,7 +32,7 @@ public class InfectEventListener {
     private void convertAndSendEmail(String accountId) {
         ServiceMessage message = new ServiceMessage();
         message.setAccountId(accountId);
-        message.addParam(API_NAME_KEY, "MajordomoVHMalwareFound");
+        message.addParam(API_NAME_KEY, "MajordomoVHScriptMailDisabled");
         message.addParam(TYPE_KEY, EMAIL);
         message.addParam(PRIORITY_KEY, 10);
         personmgr.sendNotificationToClient(message);
